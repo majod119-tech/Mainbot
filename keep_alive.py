5d7030f7f96fe8f46869650b37d9a396cbaa5e4c
majod119-tech <majod.119@gmail.com>
2026-02-15 20:28:42 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,8 +9,9 @@ def home():
     return "Bot is running on Render!"
 
 def run():
-    # Render يعطي البورت تلقائياً، وإلا سيستخدم 8080
-    app.run(host='0.0.0.0', port=8080)
+    # هنا السطر السحري: سيقرأ البورت من Render تلقائياً، وإلا سيضع 8080
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
